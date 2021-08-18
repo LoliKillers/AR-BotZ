2cbe84e53e27b840816f5f1626adb53b2161aec4
Loli Killers <80396688+LoliKillers@users.noreply.github.com>
2021-08-18 12:39:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -511,16 +511,9 @@ case 'jooxsearch':
                 case 'infocuaca':
                     if (args.length == 0) return reply(`Example: ${prefix + command} jawa timur`)
                     provinsi = args.join(" ")
-                    await fetchJson(`https://arnz-api.herokuapp.com/api/infocuaca?provinsi=${provinsi}`)
-                            .then(response => response.json())
-        .then(data => {
-        var result = data;
-             res.json({
-                 result
-             })
-         })
-lolikiller.sendMessage(from, result, text,{quoted: kill})
-
+                    get_resultt = await fetchJson(`https://arnz-api.herokuapp.com/api/infocuaca?provinsi=${provinsi}`)
+                    ini_txt = `${get_result.result}`
+                    fakegroup(ini_txt)
                     break
 case 'tovn':
 		 	  if ((isMedia && !kill.message.videoMessage || isQuotedImage) && args.length == 0) {
